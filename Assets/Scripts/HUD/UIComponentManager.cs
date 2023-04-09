e971af2f2f96ce0b902692c00c9ed30e6b44a180
Vitor <vitoraugusto_14@outlook.com>
2023-04-09 16:50:17 -0300

    dialogue sounds

---
+++ Assets/Scripts/HUD/UIComponentManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIComponentManager : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler
+{
+    public AudioClip hoverAudio, selectAudio;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        AudioManager.Instance.PlayOneShot2D(selectAudio, AudioManager.Instance.gameObject, AudioManager.AudioType.SFX, 0.08f);
+        Debug.Log("clicked");
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        AudioManager.Instance.PlayOneShot2D(hoverAudio, AudioManager.Instance.gameObject, AudioManager.AudioType.SFX, 0.8f);
+    }
+}
